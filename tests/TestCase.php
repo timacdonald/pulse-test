a3d7e4c33fc4e999e764422f3cf8abfa74e51d3e
Tim MacDonald <hello@timacdonald.me>
2023-12-18 15:23:43 +1100

    Count connected threads

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\DB;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -13,5 +14,6 @@ abstract class TestCase extends BaseTestCase
         parent::setUp();
 
         file_put_contents(base_path('memory.log'), round(memory_get_usage() / 1000000, 2).PHP_EOL, FILE_APPEND);
+        file_put_contents(base_path('connected-threads.log'), DB::select("show status where `variable_name` = 'Threads_connected';")[0]->Value.PHP_EOL, FILE_APPEND);
     }
 }
