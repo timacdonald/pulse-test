3a8a971dc77298860a61d93cca855fbdb0c9cbac
Tim MacDonald <hello@timacdonald.me>
2023-12-18 15:15:32 +1100

    Write memory to log

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        file_put_contents(base_path('memory.log'), round(memory_get_usage() / 1000000, 2).PHP_EOL, FILE_APPEND);
+    }
 }
